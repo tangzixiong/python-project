3b5233d7d6445bac3c170f2039efb0d018e99d4d
tangzx <tangzx@gditsec.org.cn>
2023-02-17 17:54:22 +0800

    json

--- spider codes/data storage.py
+++ spider codes/data storage.py
@@ -25,3 +25,35 @@ for item in items:
 """ with open('explore.txt', 'a', encoding='utf-8') as file:            ##如保存时将原文清空，可将第二个参数改写为 w   ##a: 以追加方式打开一个文件
     file.write('\n'.join([question, author, answer]))
     file.write('\n' + '=' * 50 + '\n') """
+    
+    
+##读取JSON
+import json
+
+str = '''
+[{
+    "name": "Bob",
+    "gender": "male",
+    "birthday": "1992-10-18"
+},{
+    "name": "Selina",
+    "gender": "female",
+    "birthday": "1995-10-18"
+}]
+'''
+print((type(str)))
+data = json.loads(str)
+print(data)
+print(type(data))
+
+##输出Json
+import json
+
+data = [{
+    'name' :'Bob',
+    'gender' : 'female',
+    'birthday': '1992-10-18'
+}]
+
+with open('data.json', 'w') as file:
+    file.write(json.dumps(data))
