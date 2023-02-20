53279d1b4f19ee7cabcd5db1c3ebef0bfb1912d7
Tangzhixiong <zixiong_tang@163.com>
2023-02-20 20:09:06 +0800

    new

---
+++ python study codes/database.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "shuke"
+# Date: 2018/5/13
+
+#### python操作MySQL模块
+
+import pymysql
+
+db = pymysql.connect("localhost", "host", "111111", "python_test")
