774aa3389eb8126d00c62ac3e40783489369b623
Tangzhixiong <zixiong_tang@163.com>
2023-02-27 23:12:47 +0800

    single list

--- data structure & algorithm/linked list.py
+++ data structure & algorithm/linked list.py
@@ -60,15 +60,81 @@ class SingleLinkList(object):
         
 #单链表 链表尾部添加元素，尾插法（append）
 
-def append(self, item):
-    """ 链表尾部添加元素 """
-    node = SingleNode(item)
-    #先判断链表是否为空，若空则将_head指向新节点
-    if self.is_empty():
-        self.__head = node
-        #若不为空，则找到尾部，将节点的next指向新节点
-    else:
-        cur = self.__head
-        while cur.next != None:
-            cur = cur.next
+    def append(self, item):
+        """ 链表尾部添加元素 """
+        node = SingleNode(item)
+        #先判断链表是否为空，若空则将_head指向新节点
+        if self.is_empty():
+            self.__head = node
+            #若不为空，则找到尾部，将节点的next指向新节点
+        else:
+            cur = self.__head
+            while cur.next != None:
+                cur = cur.next
             cur.next = node
+
+
+    #头部添加元素
+    def add(self, item):
+        """ 头部添加元素 """
+        #先创建一个保存item值的节点
+        node = SingleNode(item)
+        #将新节点的链接域next指向头节点，即_head指向的位置
+        node.next = self._head
+        #将链表的头_head指向新节点
+        self._head = node
+
+
+
+#指定位置添加元素
+    def insert(self, pos, item):
+        """ 指定位置添加元素 """
+        #若指定位置pos为第一个元素之前，则执行头部插入
+        if pos <=0:
+            self.add(item)
+        #若指定位置超过链表尾部，则执行尾部插入
+        elif pos > (self.length()-1):
+            self.append(item)
+        #找到指定位置
+        else:
+            node = SingleNode(item)
+            count = 0
+            #pre用来指向指定位置pos的前一个位置pos-1, 初始从头节点开始移动到指定位置
+            pre = self._head
+            while count < (pos-1):
+                count +=1
+                pre = pre.next
+            #先将新节点node的next指向插入位置的节点
+            node.next = pre.next
+            #将插入位置的前一个节点的next指向新节点
+            pre.next = node
+
+
+#查找节点是否存在
+
+    def search(self, item):
+        """ 链表查找节点是否存在，并返回True或者False """
+        cur = self._head
+        while cur != None:
+            if cur.item == item:
+                return True
+            cur = cur.next
+        return False
+
+
+
+######测试
+if __name__ == "__main__":
+    ll = SingleLinkList()
+    ll.add(1)
+    ll.add(2)
+    ll.append(3)
+    ll.insert(2,4)
+    print("length:",ll.length())
+    ll.travel()
+    print(ll.search(3))
+    print(ll.search(5))
+    ll.remove(1)
+    print("length", ll.length())
+    ll.travel()
+
